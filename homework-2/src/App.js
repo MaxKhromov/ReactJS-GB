7d292ba048449488ad459e55e50dc26f906328cd
MaxKhromov <sloopemail@gmail.com>
2019-07-23 01:48:04 +0400

    homework 2 is ready

--- homework-2/src/App.js
+++ homework-2/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Developer } from './classes/Developer';
+
+let developer = new Developer('Sasha', 'Senior', 'Java Developer');
 
 class App extends React.Component {
     render() {
-        return <div >
-            <
-            h1 > Первое приложение на React < /h1> <
-            p > Hello React! < /p> <
-            /div>
+        return <div>
+            <h1>Первое приложение на React</h1>
+            <p>Please welcome <b>{developer.getFullName()}</b></p> 
+            </div>
     }
 }
 
