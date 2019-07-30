05cdb6d6e94e3a87e25abb4443e1aada7899da08
Maksim Khromov <sloopemail@gmail.com>
2019-07-30 08:27:54 +0400

    doing Blog

--- homework-4/src/App.js
+++ homework-4/src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Blog from './app/components/Blog';
-import Post from './app/components/Post';
-import WelcomeModal from './app/components/WelcomeModal';
+import Blog from "./app/components/Blog";
+import Post from "./app/components/Post";
+import WelcomeModal from "./app/components/WelcomeModal";
 
+const posts = [
+  {
+    title: "Post 1",
+    body:
+      "Amet deserunt tempor esse pariatur. Excepteur dolore ex adipisicing esse Lorem. Et Lorem consectetur voluptate nostrud proident esse."
+  },
+  {
+    title: "Post 2",
+    body: `Officia duis fugiat incididunt nulla elit occaecat nostrud et dolore minim laborum sit. Consequat do duis anim fugiat labore ipsum dolor incididunt culpa occaecat deserunt officia occaecat. Labore id nulla esse aliqua id.
+
+Est qui aliquip ullamco cupidatat deserunt magna eiusmod aliquip. Do Lorem exercitation officia tempor eu ex aliqua aliqua et ipsum culpa veniam reprehenderit laboris. Dolore mollit esse fugiat pariatur ut sit veniam. Consectetur ullamco ut enim occaecat irure cupidatat incididunt eiusmod elit.
+
+Nulla laborum laborum veniam laboris enim. Duis nulla aute sit occaecat nisi eu reprehenderit ullamco aliquip tempor ex. Esse sunt incididunt aute magna nostrud eiusmod magna cupidatat culpa quis. Ut quis est laboris do nulla. Est ullamco id cupidatat adipisicing proident minim veniam. Magna pariatur elit duis elit elit nisi culpa officia adipisicing aute. Sint veniam elit cillum enim do eu id ex aliqua mollit sit.`
+  }
+];
 
 class App extends React.Component {
-    render() {
-        return <div>
-            <h1>Первое приложение на React</h1>
-            <p>Please welcome <b>{developer.getFullName()}</b></p> 
-            </div>
-    }
+  render() {
+    return (
+      <div>
+        <Blog />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render( < App / > , document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ homework-4/src/app/components/Blog.js
@@ -0,0 +1,11 @@
+class Blog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Blog;
