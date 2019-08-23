ae829c0a0a697cd2e684b5ae3d4083faa433f67e
MaxKhromov <sloopemail@gmail.com>
2019-08-24 02:02:01 +0400

    my first commit

---
+++ Redux/redux_first_try/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+      <div class="app_content">
+       <PostForm />
+       <hr></hr>
+       <Posts />
+       </div>
+    </Provider>
+    );
+  }
+}
+
+export default App;
